42a85a40ee270968d4971f9f1815b7cc6e3d2446
Panagis_Eirini <panos1eirini@gmail.com>
2024-06-06 20:20:07 +0300

    xtisame html,views doulepsame kai jinja

--- testApp/urls.py
+++ testApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("mesa", views.index, name="index"),
-    path("", views.index2, name="index"),
+    path("about", views.about, name="about"),
+    path("", views.index3, name="index"),
 ]
--- testApp/views.py
+++ testApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Question,GasTanks
 
 # Create your views here.
 from django.http import HttpResponse
@@ -10,3 +11,12 @@ def index2(request):
     return HttpResponse("Hello, world. From the app re arxidi gamimeno auto einai to index 2! You're at the polls index.")
 
 
+def about(request):
+    gasTankList = GasTanks.objects.all()
+    return render(request, "testApp/about.html",{"gasTankList":gasTankList})
+  
+
+def index3(request):
+    latest_question_list = Question.objects.all()
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "testApp/index.html", context)
